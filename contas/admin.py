f502f0d456480e017205201ea7d7817404406415
sdavidbastos <davidtavares98@gmail.com>
2020-12-29 09:26:13 -0300

    Remocao do cache

--- contas/admin.py
+++ contas/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Categoria
+from .models import Categoria, Transacao
 
 admin.site.register(Categoria)
-
+admin.site.register(Transacao)
 # Register your models here.
